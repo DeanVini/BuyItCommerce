ca65db2b69376bba747b07454983d79516e37d23
DeanVini <deanvinicius2003@gmail.com>
2026-06-23 17:40:07 -0300

    feat(infrastructure): registro de dependências da infraestrutura

---
+++ src/BuyItCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,64 @@
+using BuyItCommerce.Application.Abstractions.Caching;
+using BuyItCommerce.Application.Abstractions.Persistence;
+using BuyItCommerce.Application.Abstractions.Idempotency;
+using BuyItCommerce.Application.Abstractions.Outbox;
+using BuyItCommerce.Application.Configuration;
+using BuyItCommerce.Infrastructure.Caching;
+using BuyItCommerce.Infrastructure.Configuration;
+using BuyItCommerce.Infrastructure.Persistence;
+using BuyItCommerce.Infrastructure.Persistence.Repositories;
+using BuyItCommerce.Infrastructure.ReadModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace BuyItCommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<OutboxOptions>()
+            .Bind(configuration.GetSection(OutboxOptions.SectionName))
+            .ValidateDataAnnotations();
+
+        services.AddOptions<MongoOptions>()
+            .Bind(configuration.GetSection(MongoOptions.SectionName))
+            .ValidateDataAnnotations();
+
+        services.AddOptions<IdempotencyOptions>()
+            .Bind(configuration.GetSection(IdempotencyOptions.SectionName))
+            .ValidateDataAnnotations();
+
+        services.AddOptions<CacheOptions>()
+            .Bind(configuration.GetSection(CacheOptions.SectionName))
+            .ValidateDataAnnotations();
+
+        services.AddDbContext<OrdersDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+        services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<OrdersDbContext>());
+        services.AddScoped<IOrderWriteRepository, EfOrderWriteRepository>();
+        services.AddScoped<IIdempotencyStore, EfIdempotencyStore>();
+        services.AddScoped<IOutboxWriter, EfOutboxWriter>();
+
+        RegisterMongoSerializers();
+        services.AddSingleton<IMongoClient>(_ => new MongoClient(configuration.GetConnectionString("MongoDb")));
+        services.AddScoped<OrdersReadContext>();
+        services.AddScoped<IOrderReadRepository, MongoOrderReadRepository>();
+
+        services.AddMemoryCache();
+        services.AddSingleton<ICacheService, MemoryCacheService>();
+
+        return services;
+    }
+
+    private static void RegisterMongoSerializers()
+    {
+        BsonSerializer.TryRegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+    }
+}
